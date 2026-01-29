dc9a60a82bb41e5a78d7e65e6b34044aa10fe8d4
sean860120 <sean860120@gmail.com>
2026-01-29 22:36:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.post('/', async (req, res) => {
     const currentMax = parseInt((getRes.data.values?.[0]?.[0] || '0'), 10);
 
     if (bidAmount <= currentMax) {
-      replyText = '很抱歉，您的出價未高於當前最高出價';
+      replyText = '很抱歉，您的出價無效（目前最高出價：${bidAmount} 元）';
     } else {
       // 高於目前最高出價，登錄 A/B
       await sheets.spreadsheets.values.append({
@@ -78,7 +78,7 @@ app.post('/', async (req, res) => {
       });
       */
 
-      replyText = `已收到您的出價：${bidAmount} 元（目前最高出價：${bidAmount} 元）`;
+      replyText = `已收到您的出價：${bidAmount} 元`;
     }
 
   } catch (err) {
